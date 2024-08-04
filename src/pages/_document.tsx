4b721fb28c80e6d999e3388855b4383a01a2bf4c
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-04 17:15:18 +0900

    メタ情報の設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const faviconPath = "favicon";
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${faviconPath}/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${faviconPath}/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${faviconPath}/favicon-16x16.png`}
+        />
+        <link rel="manifest" href={`${faviconPath}/site.webmanifest`} />
+        <meta name="theme-color" content="#000000" key="themeColor" />
+      </Head>
       <body>
         <Main />
         <NextScript />
