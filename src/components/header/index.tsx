2b6abff817e05dec2687b587dc2821ded8bf9ffa
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-10 23:45:40 +0900

    PCサイズ headerの作成

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,64 @@
+import Link from "next/link";
 import type { FC } from "react";
 
+import styles from "./index.module.scss";
+
+const navItems = [
+  {
+    label: "3S codeについて",
+    href: "/",
+  },
+  {
+    label: "サービス",
+    href: "/",
+  },
+  {
+    label: "制作実績",
+    href: "/",
+  },
+  {
+    label: "料金",
+    href: "/",
+  },
+  {
+    label: "ブログ",
+    href: "/",
+  },
+  {
+    label: "お問い合わせ",
+    href: "/",
+  },
+];
+
 const Header: FC = () => {
-  return <header>{/* <p>ヘッダーです</p> */}</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <h1>
+          <Link href="/" className={styles.titleLink}>
+            3S code
+          </Link>
+        </h1>
+        <nav>
+          <ul className={styles.navLists}>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className={styles.navLink}
+                  data-contact-button={
+                    item.label === "お問い合わせ" || undefined
+                  }
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
