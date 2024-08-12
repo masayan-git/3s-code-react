4ec38ac50d9639009807584b2f6070b558df94e7
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-12 21:48:31 +0900

    promiseブロックの作成

--- src/components/top/about/index.tsx
+++ src/components/top/about/index.tsx
@@ -4,6 +4,23 @@ import SectionTitle from "../sectionTitle";
 import styles from "./index.module.scss";
 
 const TopAbout = () => {
+  const promiseItems = [
+    {
+      title: "納期厳守",
+      content:
+        "貴社のパートナーとしてプロジェクトの一端をお任せ頂いていることを強く意識し、納期を厳守致します。",
+    },
+    {
+      title: "報告・連絡・相談・再確認",
+      content:
+        "報告・連絡・相談に加えて再確認を行います。打ち合わせの議事録を取り、認識のずれがないか再確認をさせていただきます。",
+    },
+    {
+      title: "不明点を自己完結しない",
+      content:
+        "ご担当者様の意図しない実装をしないためにも不明点を明確にしご質 問の上対応いたします。 を強く意識し、納期を厳守致します。",
+    },
+  ];
   return (
     <section className={styles.topAbout}>
       <div className={styles.inner}>
@@ -15,7 +32,7 @@ const TopAbout = () => {
             <div className={styles.massageImageArea}>
               <div className={styles.massageImage}>
                 <Image
-                  src="/top/about/about-1.jpg"
+                  src="/top/about/about-1.webp"
                   width={720}
                   height={510}
                   alt=""
@@ -34,6 +51,36 @@ const TopAbout = () => {
                 </p>
               </div>
               <p className={styles.messageSign}>3S code 代表 庄司雅幸</p>
+            </div>
+          </div>
+          <div className={styles.promise}>
+            <div className={styles.promiseContent}>
+              <h3 className={styles.promiseTitle}>３つのお約束</h3>
+              <div className={styles.promiseItems}>
+                {promiseItems.map((item, index) => (
+                  <div key={index} className={styles.promiseItem}>
+                    <h4 className={styles.promiseItemTitle}>
+                      <span className={styles.promiseItemLabel}>
+                        PROMISE / 0{index + 1}
+                      </span>
+                      <span className={styles.promiseItemTitleText}>
+                        {item.title}
+                      </span>
+                    </h4>
+                    <p className={styles.promiseItemText}>{item.content}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={styles.promiseImageArea}>
+              <div className={styles.promiseImage}>
+                <Image
+                  src={"/top/about/about-2.webp"}
+                  width={440}
+                  height={506}
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
