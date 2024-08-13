cedcca7f7f8a334da9b147eef7b14a3d27fcee0b
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-13 23:11:12 +0900

    トップ worksセクションの作成

---
+++ src/components/works-card/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import styles from "./index.module.scss";
+
+const WorksCard = () => {
+  return (
+    <article className={styles.worksCard}>
+      <Link href="/" className={styles.link}>
+        <div className={styles.image}>
+          <img
+            src="https://via.placeholder.com/300"
+            width={300}
+            height={300}
+            alt="サイトタイトル"
+          />
+        </div>
+        <div className={styles.titleArea}>
+          <h1 className={styles.title}>タイトルです</h1>
+          <p className={styles.urlText}>https://via.placeholder.com/300</p>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default WorksCard;
