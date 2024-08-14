5ca63ba3cfbc1842b327df2095eec5e0999ccbb6
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-14 22:59:15 +0900

    top contactセクションの作成

---
+++ src/components/top/contact/index.tsx
@@ -0,0 +1,30 @@
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import styles from "./index.module.scss";
+
+const TopContact = () => {
+  return (
+    <section className={styles.topContact}>
+      <h2 className={styles.title}>CONTACT</h2>
+      <div className={styles.contents}>
+        <p className={styles.mainText}>
+          <span>Webコーディング業務の</span>
+          <span>お問い合わせはこちら</span>
+        </p>
+        <p className={styles.subText}>
+          <span>お気軽にご相談ください。</span>
+          <span>24時間以内に返答させていただきます。</span>
+        </p>
+        <div className={styles.buttonArea}>
+          <button className={styles.button}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+            お問い合わせする
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopContact;
