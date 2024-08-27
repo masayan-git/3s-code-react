11c67d3b3924bd89798bd27d081b56b57036bd28
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-27 23:53:49 +0900

    worksページをapp routerに対応させる

--- src/layouts/works/index.tsx
+++ src/layouts/works/index.tsx
@@ -1,19 +1,15 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
 import PageTitle from "@/components/pageTitle";
 import Cta from "@/components/top/cta";
 import type { LayoutProps } from "@/types";
 
-const WorksLayout = ({ children }: LayoutProps) => {
+const WorksLayoutContainer = ({ children }: LayoutProps) => {
   return (
     <>
-      <Header />
       <PageTitle titleEn="Works" titleJa="制作実績" />
       <main>{children}</main>
       <Cta theme="light" />
-      <Footer />
     </>
   );
 };
 
-export default WorksLayout;
+export default WorksLayoutContainer;
