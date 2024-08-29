a4ebb5bcf7f436d6fb8a33b18f27b2e3695e8ae1
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-29 21:14:43 +0900

    フォームをapp routerに対応させる

--- src/layouts/contact/index.tsx
+++ src/layouts/contact/index.tsx
@@ -1,17 +1,13 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
 import PageTitle from "@/components/pageTitle";
 import type { LayoutProps } from "@/types";
 
-const ContactLayout = ({ children }: LayoutProps) => {
+const ContactLayoutContainer = ({ children }: LayoutProps) => {
   return (
     <>
-      <Header />
       <PageTitle titleEn="Contact" titleJa="お問い合わせ" />
       <main>{children}</main>
-      <Footer />
     </>
   );
 };
 
-export default ContactLayout;
+export default ContactLayoutContainer;
