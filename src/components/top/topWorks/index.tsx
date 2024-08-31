7eed21f64be77bb9437824c551aef8a0d6f2548d
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-08-31 15:12:40 +0900

    トップページ blog worksコンテンツをmicrocmsから取得する実装

--- src/components/top/topWorks/index.tsx
+++ src/components/top/topWorks/index.tsx
@@ -3,20 +3,24 @@ import WorksCard from "@/components/worksCard";
 
 import SectionTitle from "../sectionTitle";
 import styles from "./index.module.scss";
+import { getWorksList } from "@/libs/microcms";
 
-const TopWorks = () => {
+const TopWorks = async () => {
+  const data = await getWorksList({
+    limit: 3,
+  });
   return (
     <section className={styles.container}>
       <div className={styles.bgInner}>
         <div className={styles.inner}>
           <SectionTitle titleEn="WORKS" titleJa="制作実績" />
           <div className={styles.cards}>
-            <WorksCard />
-            <WorksCard />
-            <WorksCard />
+            {data.contents.map((works) => (
+              <WorksCard key={works.id} data={works} />
+            ))}
           </div>
           <div className={styles.buttonArea}>
-            <MoreButton href="/" text="実績一覧を見る" />
+            <MoreButton href="/works" text="実績一覧を見る" />
           </div>
         </div>
       </div>
