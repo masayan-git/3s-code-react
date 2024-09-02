ed56eb195bc69505df19732352df34f6de5a5297
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-09-02 23:48:42 +0900

    blogにページネーションを追加

--- src/app/blog/category/[id]/p/[current]/page.tsx
+++ src/app/blog/category/[id]/p/[current]/page.tsx
@@ -7,37 +7,46 @@ import { getBlogList, getCategoryDetail } from "@/libs/microcms";
 
 import styles from "./index.module.scss";
 
+export const revalidate = 1;
+
 type Props = {
   params: {
     id: string;
+    current: string;
   };
 };
 
-export const revalidate = 1;
+const page = async ({ params }: Props) => {
+  const current = parseInt(params.current, 10);
 
-const CategoryArchive = async ({ params }: Props) => {
+  if (Number.isNaN(current) || current < 1) {
+    notFound();
+  }
+
   const category = await getCategoryDetail(params.id).catch(notFound);
+
   const { contents: article, totalCount } = await getBlogList({
     filters: `category[equals]${category.id}`,
     limit: BLOG_LIST_LIMIT,
+    offset: BLOG_LIST_LIMIT * (current - 1),
   });
-  if (!article) return <p>記事がありません</p>;
+
+  if (article.length === 0) {
+    notFound();
+  }
 
   return (
     <div className={styles.container}>
-      <h2>
-        <span className={styles.category}>カテゴリー</span>
-        <span className={styles.categoryName}>{category.name}</span>
-      </h2>
       {article.map((blog) => (
         <BlogCard key={blog.id} blog={blog} />
       ))}
       <Pagination
         totalCount={totalCount}
+        current={current}
         basePath={`/blog/category/${category.id}`}
       />
     </div>
   );
 };
 
-export default CategoryArchive;
+export default page;
